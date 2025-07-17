3a7734b2cae80a71688ac1008f9c080704aca392
Francisco Mota <franciscolmota@outlook.com>
2025-07-17 17:01:44 -0400

    Dark mode

--- src/components/FallingText.tsx
+++ src/components/FallingText.tsx
@@ -201,7 +201,7 @@ export function FallingText({ darkMode }: cubeProps) {
   return (
     <section id="hero">
       <div className="text-midnight dark:text-eggshell">
-        <div className="h-screen m-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 text-center">
+        <div className="h-screen m-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-12 text-center pt-0">
           <div
             ref={containerRef}
             style={{
